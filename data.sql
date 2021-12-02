78f6a522e6612723935dc6c1ef1a2f33d889eca3
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-02 19:04:35 +0200

    Insert data inside the species table and Copy the SQL into the data.sql file

--- data.sql
+++ data.sql
@@ -19,3 +19,6 @@ INSERT INTO owners (full_name, age) VALUES('Bob', 45);
 INSERT INTO owners (full_name, age) VALUES('Melody Pond', 77);
 INSERT INTO owners (full_name, age) VALUES('Dean Winchester', 14);
 INSERT INTO owners (full_name, age) VALUES('Jodie Whittaker', 38);
+/* Insert the following data into the species table: */
+INSERT INTO species (name) VALUES('Pokemon');
+INSERT INTO species (name) VALUES('Digimon');
