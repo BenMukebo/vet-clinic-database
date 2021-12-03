8aa6cbd628a24d48b51fabfdd0923d1b40e85fd6
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-03 17:36:28 +0200

    Vet Jack Harkness is specialized in Digimon

--- data.sql
+++ data.sql
@@ -65,4 +65,4 @@ INSERT INTO specializations (species_id, vets_id)
 VALUES ((SELECT id FROM species WHERE name = 'Pokemon'), (SELECT id FROM vets WHERE name = 'William Tatcher')),
 	   ((SELECT id FROM species WHERE name = 'Pokemon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez')),
        ((SELECT id FROM species WHERE name = 'Digimon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez')),
-	  
+	   ((SELECT id FROM species WHERE name = 'Digimon'), (SELECT id FROM vets WHERE name = 'Jack Harkness'))
