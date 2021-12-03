0f0d246e8bc129f2906fee78bae7b599a29c90a3
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-03 19:27:19 +0200

    How many different animals did Stephanie Mendez see?

--- queries.sql
+++ queries.sql
@@ -119,3 +119,9 @@ WHERE vets.name = 'William Tatcher'
 ORDER BY visits.date_of_visit DESC
 LIMIT 1;
 
+-- How many different animals did Stephanie Mendez see?
+SELECT COUNT(*) as num_animals_visited FROM vets
+JOIN visits ON vets.id = visits.vets_id
+WHERE vets.name = 'Stephanie Mendez';
+
+
