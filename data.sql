01fc07470b0508d3b5d6f46f4777b50ecc618a83
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-03 17:35:52 +0200

    Vet Stephanie Mendez is specialized in Digimon

--- data.sql
+++ data.sql
@@ -64,4 +64,5 @@ INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, da
 INSERT INTO specializations (species_id, vets_id)
 VALUES ((SELECT id FROM species WHERE name = 'Pokemon'), (SELECT id FROM vets WHERE name = 'William Tatcher')),
 	   ((SELECT id FROM species WHERE name = 'Pokemon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez')),
-      
+       ((SELECT id FROM species WHERE name = 'Digimon'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez')),
+	  
