e08a126e224c7735c147ba5dfc0069001e26b272
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-12-07 17:36:03 +0200

    Use EXPLAIN ANALYZE on the previous queries to check what is happening

--- queries.sql
+++ queries.sql
@@ -180,3 +180,9 @@ JOIN animals ON animals.id = visits.animals_id
 JOIN species ON species.id = animals.species_id
 WHERE vets.name = 'Maisy Smith'
 GROUP BY species.name;
+
+
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animals_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vets_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
